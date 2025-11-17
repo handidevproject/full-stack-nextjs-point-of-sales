be3935cf9cf3f48c5e856c8833de3528a9a7ce29
Handi <handikadwiputradev@gmail.com>
2025-11-17 22:14:54 +0700

    feat: Implement dynamic sidebar and landing page
    - Mengimplementasikan sidebar dinamis yang menampilkan item menu berdasarkan peran pengguna.
    - Memperbarui halaman landing dengan pesan selamat datang dan tautan ke dasbor.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { DarkmodeToggle } from '@/components/common/darkmode-toggle';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div>
-            <Button className="bg-red-400 dark:bg-amber-300">Hello</Button>
-            <DarkmodeToggle />
+        <div className="bg-muted flex justify-center items-center h-screen flex-col space-y-4">
+            <h1 className="text-4xl font-semibold">Welcome Handika Dwiputra</h1>
+            <Link href="/admin">
+                <Button className="bg-teal-500 text-white">Access Dashboard</Button>
+            </Link>
         </div>
     );
 }
