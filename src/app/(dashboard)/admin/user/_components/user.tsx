0deb429350de0bf838147d6996af38ac7dd0ad51
Handi <handikadwiputradev@gmail.com>
2025-11-19 05:01:24 +0700

    feat(user): Implement data table for user management
    - Mengganti tampilan daftar pengguna statis dengan komponen `DataTable` yang lebih terstruktur.
    - Mengintegrasikan `useQuery` untuk mengambil data pengguna dari Supabase secara asinkron.
    - Menambahkan `useMemo` untuk memformat dan mempersiapkan data sebelum ditampilkan di tabel.
    - Menyediakan umpan balik loading dan notifikasi error menggunakan `sonner`.

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -1,34 +1,46 @@
-'use client'
+'use client';
 
-import {Input} from "@/components/ui/input";
-import {Dialog, DialogTrigger} from "@/components/ui/dialog";
-import {Button} from "@/components/ui/button";
-import {useQuery} from "@tanstack/react-query";
-import {createClient} from "@/lib/supabase/client";
-import {toast} from "sonner";
+import DataTable from '@/components/common/data-table';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { HEADER_TABLE_USER } from '@/constants/user-constant';
+import { createClient } from '@/lib/supabase/client';
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { toast } from 'sonner';
 
 export default function UserManagement() {
     const supabase = createClient();
-    const {data: users, isLoading } = useQuery({
+    const { data: users, isLoading } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
-            const {data, error} = await supabase
+            const { data, error } = await supabase
                 .from('profiles')
-                .select('*',{count: 'exact'})
+                .select('*', { count: 'exact' })
                 .order('created_at');
+
             if (error)
-                toast.error('Get Users data Failed', {
+                toast.error('Get User data failed', {
                     description: error.message,
                 });
+
             return data;
-        }
+        },
     });
+
+    const filteredData = useMemo(() => {
+        return (users || []).map((user, index) => {
+            return [index + 1, user.id, user.name, user.role, ''];
+        });
+    }, [users]);
+
     return (
         <div className="w-full">
-            <div className="flex flex-col lg:flex-row mb-4 gap-4 justify-between w-full">
+            <div className="flex flex-col lg:flex-row mb-4 gap-2 justify-between w-full">
                 <h1 className="text-2xl font-bold">User Management</h1>
                 <div className="flex gap-2">
-                    <Input type="text" placeholder="Search by Name"/>
+                    <Input placeholder="Search by name" />
                     <Dialog>
                         <DialogTrigger asChild>
                             <Button variant="outline">Create</Button>
@@ -36,13 +48,11 @@ export default function UserManagement() {
                     </Dialog>
                 </div>
             </div>
-            {isLoading && <div>Loading...</div>}
-            {users?.map((user) => (
-                <div key={user.id}>
-                    <h2>{user.name}</h2>
-                    <h2>{user.role}</h2>
-                </div>
-            ))}
+            <DataTable
+                header={HEADER_TABLE_USER}
+                data={filteredData}
+                isLoading={isLoading}
+            />
         </div>
     );
 }
