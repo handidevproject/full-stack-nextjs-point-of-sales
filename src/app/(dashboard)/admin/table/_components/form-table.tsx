140cfc95088aed3dd7aa328e28c27b5f527bc3c4
Handi <handikadwiputradev@gmail.com>
2025-11-20 20:01:33 +0700

    feat(delete-table) - Added function delete table.

--- src/app/(dashboard)/admin/table/_components/form-table.tsx
+++ src/app/(dashboard)/admin/table/_components/form-table.tsx
@@ -10,7 +10,6 @@ import {
     DialogTitle,
 } from '@/components/ui/dialog';
 import { Form } from '@/components/ui/form';
-
 import { STATUS_TABLE_LIST } from '@/constants/table-constant';
 import { Loader2 } from 'lucide-react';
 import { FormEvent } from 'react';
@@ -30,51 +29,51 @@ export default function FormTable<T extends FieldValues>({
     return (
         <DialogContent className="sm:max-w-[425px] max-h-[90vh]">
             <Form {...form}>
-            <DialogHeader>
-                <DialogTitle>{type} Table</DialogTitle>
-    <DialogDescription>
-    {type === 'Create' ? 'Add a new table' : 'Make changes table here'}
-    </DialogDescription>
-    </DialogHeader>
-    <form onSubmit={onSubmit} className="space-y-4">
-    <div className="space-y-4 max-h-[50vh] px-1 overflow-y-auto">
-    <FormInput
-        form={form}
-    name={'name' as Path<T>}
-    label="Name"
-    placeholder="Insert name here"
-    />
-    <FormInput
-        form={form}
-    name={'description' as Path<T>}
-    label="Description"
-    placeholder="Insert description here"
-    type="textarea"
-    />
-    <FormInput
-        form={form}
-    name={'capacity' as Path<T>}
-    label="Capacity"
-    placeholder="Insert capacity here"
-    type="number"
-    />
-    <FormSelect
-        form={form}
-    name={'status' as Path<T>}
-    label="Status"
-    selectItem={STATUS_TABLE_LIST}
-    />
-    </div>
-    <DialogFooter>
-    <DialogClose asChild>
-    <Button variant="outline">Cancel</Button>
-        </DialogClose>
-        <Button type="submit">
-        {isLoading ? <Loader2 className="animate-spin" /> : type}
-        </Button>
-        </DialogFooter>
-        </form>
-        </Form>
+                <DialogHeader>
+                    <DialogTitle>{type} Table</DialogTitle>
+                    <DialogDescription>
+                        {type === 'Create' ? 'Add a new table' : 'Make changes table here'}
+                    </DialogDescription>
+                </DialogHeader>
+                <form onSubmit={onSubmit} className="space-y-4">
+                    <div className="space-y-4 max-h-[50vh] px-1 overflow-y-auto">
+                        <FormInput
+                            form={form}
+                            name={'name' as Path<T>}
+                            label="Name"
+                            placeholder="Insert name here"
+                        />
+                        <FormInput
+                            form={form}
+                            name={'description' as Path<T>}
+                            label="Description"
+                            placeholder="Insert description here"
+                            type="textarea"
+                        />
+                        <FormInput
+                            form={form}
+                            name={'capacity' as Path<T>}
+                            label="Capacity"
+                            placeholder="Insert capacity here"
+                            type="number"
+                        />
+                        <FormSelect
+                            form={form}
+                            name={'status' as Path<T>}
+                            label="Status"
+                            selectItem={STATUS_TABLE_LIST}
+                        />
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">
+                            {isLoading ? <Loader2 className="animate-spin" /> : type}
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </Form>
         </DialogContent>
-);
+    );
 }
