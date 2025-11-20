84a52381a1654f10ab4a01722bfbc42372329567
Handi <handikadwiputradev@gmail.com>
2025-11-20 19:58:07 +0700

    feat(update-table) - Added function update table

--- src/app/(dashboard)/admin/table/_components/table.tsx
+++ src/app/(dashboard)/admin/table/_components/table.tsx
@@ -14,6 +14,8 @@ import { toast } from 'sonner';
 import { cn } from '@/lib/utils';
 import { Table } from '@/validations/table-validation';
 import { HEADER_TABLE_TABLE } from '@/constants/table-constant';
+import DialogCreateTable from './dialog-create-table';
+import DialogUpdateTable from './dialog-update-table';
 
 export default function TableManagement() {
     const supabase = createClient();
@@ -138,6 +140,7 @@ export default function TableManagement() {
                         <DialogTrigger asChild>
                             <Button variant="outline">Create</Button>
                         </DialogTrigger>
+                        <DialogCreateTable refetch={refetch} />
                     </Dialog>
                 </div>
             </div>
@@ -150,6 +153,12 @@ export default function TableManagement() {
                 currentLimit={currentLimit}
                 onChangePage={handleChangePage}
                 onChangeLimit={handleChangeLimit}
+            />
+            <DialogUpdateTable
+                open={selectedAction !== null && selectedAction.type === 'update'}
+                refetch={refetch}
+                currentData={selectedAction?.data}
+                handleChangeAction={handleChangeAction}
             />
         </div>
     );
--- src/app/(dashboard)/admin/table/actions.tsx
+++ src/app/(dashboard)/admin/table/actions.tsx
@@ -8,7 +8,7 @@ export async function createTable(
     prevState: TableFormState,
     formData: FormData,
 ) {
-    const validatedFields = tableSchema.safeParse({
+    let validatedFields = tableSchema.safeParse({
         name: formData.get('name'),
         description: formData.get('description'),
         capacity: parseInt(formData.get('capacity') as string),
@@ -33,6 +33,54 @@ export async function createTable(
         capacity: validatedFields.data.capacity,
         status: validatedFields.data.status,
     });
+
+    if (error) {
+        return {
+            status: 'error',
+            errors: {
+                ...prevState.errors,
+                _form: [error.message],
+            },
+        };
+    }
+
+    return {
+        status: 'success',
+    };
+}
+
+export async function updateTable(
+    prevState: TableFormState,
+    formData: FormData,
+) {
+    let validatedFields = tableSchema.safeParse({
+        name: formData.get('name'),
+        description: formData.get('description'),
+        capacity: parseInt(formData.get('capacity') as string),
+        status: formData.get('status'),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            status: 'error',
+            errors: {
+                ...validatedFields.error.flatten().fieldErrors,
+                _form: [],
+            },
+        };
+    }
+
+    const supabase = await createClient();
+
+    const { error } = await supabase
+        .from('tables')
+        .update({
+            name: validatedFields.data.name,
+            description: validatedFields.data.description,
+            capacity: validatedFields.data.capacity,
+            status: validatedFields.data.status,
+        })
+        .eq('id', formData.get('id'));
 
     if (error) {
         return {
