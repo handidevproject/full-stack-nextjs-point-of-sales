9249b04ca34060c775895eb92ee4988d336d82d6
Handi <handikadwiputradev@gmail.com>
2025-11-20 11:14:43 +0700

    feat(menu): Create menu management page
    Membuat file halaman baru di `app/(dashboard)/admin/menu/page.tsx` untuk me-render komponen `MenuManagement`.
    
    Halaman ini juga menyertakan metadata judul (`title`) untuk SEO dan pengalaman pengguna yang lebih baik.

---
+++ src/app/(dashboard)/admin/menu/page.tsx
@@ -0,0 +1,9 @@
+import MenuManagement from './_components/menu';
+
+export const metadata = {
+    title: 'SINYO Cafe | Menu Management',
+};
+
+export default function MenuManagementPage() {
+    return <MenuManagement />;
+}
