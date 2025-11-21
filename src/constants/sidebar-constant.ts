bf36e6713fbda28778b7b09ded77130db320bb40
Handi <handikadwiputradev@gmail.com>
2025-11-21 23:39:00 +0700

    feat(sidebar): Add transaction history link
    This commit adds a new "Transaction" navigation item to the application sidebar. The link, accompanied by a history icon, directs users to the `/transaction` page, providing easy access to their transaction history.

--- src/constants/sidebar-constant.ts
+++ src/constants/sidebar-constant.ts
@@ -7,7 +7,7 @@ import {
 } from 'lucide-react';
 
 export const SIDEBAR_MENU_LIST = {
-    Administrator: [
+    admin: [
         {
             title: 'Dashboard',
             url: '/admin',
@@ -34,8 +34,20 @@ export const SIDEBAR_MENU_LIST = {
             icon: Users,
         },
     ],
-    cashier: [],
-    kitchen: [],
+    cashier: [
+        {
+            title: 'Order',
+            url: '/order',
+            icon: Album,
+        },
+    ],
+    kitchen: [
+        {
+            title: 'Order',
+            url: '/order',
+            icon: Album,
+        },
+    ],
 };
 
 export type SidebarMenuKey = keyof typeof SIDEBAR_MENU_LIST;
